6989eea28fb0a2352053e4160d78ef204f1fc581
Richik SC <sc.richik@gmail.com>
2018-10-08 20:18:02 -0500

    Write basic TeleOp

--- src/main/java/frc/team1816/robot/Components.java
+++ src/main/java/frc/team1816/robot/Components.java
@@ -1,10 +1,26 @@
 package frc.team1816.robot;
 
+import frc.team1816.robot.subsystems.Drivetrain;
+
 public class Components {
     private static Components instance;
 
+    public Drivetrain drivetrain;
+
+    //Drivetrain Talon Id's
+    private final int LEFT_MAIN = 1;
+    private final int LEFT_SLAVE_ONE = 2;
+    private final int LEFT_SLAVE_TWO = 3;
+    private final int RIGHT_MAIN = 5;
+    private final int RIGHT_SLAVE_ONE = 6;
+    private final int RIGHT_SLAVE_TWO = 7;
+
+
     private Components(){
-        
+        this.drivetrain = new Drivetrain(
+            LEFT_MAIN, LEFT_SLAVE_ONE, LEFT_SLAVE_TWO,
+            RIGHT_MAIN, RIGHT_SLAVE_ONE, RIGHT_SLAVE_TWO
+        );
     }
 
     public static Components getInstance(){
--- src/main/java/frc/team1816/robot/Robot.java
+++ src/main/java/frc/team1816/robot/Robot.java
@@ -1,10 +1,26 @@
 package frc.team1816.robot;
 
+import com.edinarobotics.utils.gamepad.Gamepad;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import frc.team1816.robot.commands.GamepadDriveCommand;
+import frc.team1816.robot.subsystems.Drivetrain;
 
 public class Robot extends TimedRobot {
+
+    private Drivetrain drivetrain;
+    private Gamepad gamepadDriver;
+
     @Override
-    public void robotInit() { }
+    public void robotInit() {
+        // Initializes components and controls,
+        // which are needed to initialize subsystems
+        Components.getInstance();
+        Controls.getInstance();
+
+        drivetrain = Components.getInstance().drivetrain;
+        gamepadDriver = Controls.getInstance().gamepadDriver;
+    }
 
     @Override
     public void disabledInit() { }
@@ -13,7 +29,9 @@ public class Robot extends TimedRobot {
     public void autonomousInit() { }
 
     @Override
-    public void teleopInit() { }
+    public void teleopInit() {
+        drivetrain.setDefaultCommand(new GamepadDriveCommand(gamepadDriver));
+    }
 
     @Override
     public void testInit() { }
@@ -26,7 +44,9 @@ public class Robot extends TimedRobot {
     public void autonomousPeriodic() { }
 
     @Override
-    public void teleopPeriodic() { }
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
     @Override
     public void testPeriodic() { }

abf6f51891615abd5a4199ca2f337359ab4ecb23
Richik SC <sc.richik@gmail.com>
2018-10-25 18:31:37 -0500

    Update Talon IDs and remove second slave Talons

--- src/main/java/frc/team1816/robot/Components.java
+++ src/main/java/frc/team1816/robot/Components.java
@@ -8,18 +8,16 @@ public class Components {
     public Drivetrain drivetrain;
 
     //Drivetrain Talon Id's
-    private final int LEFT_MAIN = 1;
-    private final int LEFT_SLAVE_ONE = 2;
-    private final int LEFT_SLAVE_TWO = 3;
+    private final int LEFT_MAIN = 14;
+    private final int LEFT_SLAVE_ONE = 13;
     private final int RIGHT_MAIN = 5;
-    private final int RIGHT_SLAVE_ONE = 6;
-    private final int RIGHT_SLAVE_TWO = 7;
+    private final int RIGHT_SLAVE_ONE = 4;
 
 
     private Components(){
         this.drivetrain = new Drivetrain(
-            LEFT_MAIN, LEFT_SLAVE_ONE, LEFT_SLAVE_TWO,
-            RIGHT_MAIN, RIGHT_SLAVE_ONE, RIGHT_SLAVE_TWO
+            LEFT_MAIN, LEFT_SLAVE_ONE,
+            RIGHT_MAIN, RIGHT_SLAVE_ONE
         );
     }
 
--- src/main/java/frc/team1816/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team1816/robot/subsystems/Drivetrain.java
@@ -8,42 +8,35 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Drivetrain extends Subsystem {
 
   private TalonSRX
-      leftMain, leftSlaveOne, leftSlaveTwo,
-      rightMain, rightSlaveOne, rightSlaveTwo;
+      leftMain, leftSlaveOne,
+      rightMain, rightSlaveOne;
 
   private double leftPower = 0;
   private double rightPower = 0;
 
   public Drivetrain(
-      int leftMain, int leftSlaveOne, int leftSlaveTwo,
-      int rightMain, int rightSlaveOne, int rightSlaveTwo
+      int leftMain, int leftSlaveOne,
+      int rightMain, int rightSlaveOne
   ) {
     super();
     this.leftMain = new TalonSRX(leftMain);
     this.leftSlaveOne = new TalonSRX(leftSlaveOne);
-    this.leftSlaveTwo = new TalonSRX(leftSlaveTwo);
 
     this.rightMain = new TalonSRX(rightMain);
     this.rightSlaveOne = new TalonSRX(rightSlaveOne);
-    this.rightSlaveTwo = new TalonSRX(rightSlaveTwo);
 
     this.leftMain.setInverted(true);
     this.leftSlaveOne.setInverted(true);
-    this.leftSlaveTwo.setInverted(true);
 
     this.leftMain.setNeutralMode(NeutralMode.Brake);
     this.leftSlaveOne.setNeutralMode(NeutralMode.Brake);
-    this.leftSlaveTwo.setNeutralMode(NeutralMode.Brake);
 
     this.rightMain.setNeutralMode(NeutralMode.Brake);
     this.rightSlaveOne.setNeutralMode(NeutralMode.Brake);
-    this.rightSlaveTwo.setNeutralMode(NeutralMode.Brake);
 
     this.leftSlaveOne.set(ControlMode.Follower, leftMain);
-    this.leftSlaveTwo.set(ControlMode.Follower, leftMain);
 
     this.rightSlaveOne.set(ControlMode.Follower, rightMain);
-    this.rightSlaveTwo.set(ControlMode.Follower, rightMain);
   }
 
   public void setDrivetrain(double leftPower, double rightPower){

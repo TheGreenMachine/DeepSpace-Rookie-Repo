d371510edd3d76e4fd5a6664e730ac92e73e3daa
Richik SC <sc.richik@gmail.com>
2018-11-01 19:06:16 -0500

    Remove Xbox-specific filter classes
    The Xbox-specific filter classes are not necessary anymore as the
    `XboxGamepadAxisState` has been refactored and standard filters can
    filter Xbox gamepad values.

--- src/main/java/com/edinarobotics/utils/gamepad/gamepadfilters/XboxGamepadFilter.java
+++
@@ -1,8 +0,0 @@
-package com.edinarobotics.utils.gamepad.gamepadfilters;
-
-import com.edinarobotics.utils.gamepad.XboxGamepadAxisState;
-
-public interface XboxGamepadFilter {
-
-    public XboxGamepadAxisState filter(XboxGamepadAxisState toFilter);
-}

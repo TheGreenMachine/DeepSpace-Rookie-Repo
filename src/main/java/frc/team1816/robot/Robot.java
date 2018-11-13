d216d264b2c1bf57ef41635da6dfb3687005602d
Richik SC <sc.richik@gmail.com>
2018-11-12 19:07:49 -0600

    Write robot class

--- src/main/java/frc/team1816/robot/Robot.java
+++ src/main/java/frc/team1816/robot/Robot.java
@@ -1,16 +1,26 @@
 package frc.team1816.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import frc.team1816.robot.commands.XboxTestCommand;
 
 public class Robot extends TimedRobot {
+
+    XboxTestCommand command;
     @Override
-    public void robotInit() { }
+    public void robotInit() {
+        Components.getInstance();
+        Controls.getInstance();
+    }
 
     @Override
     public void disabledInit() { }
 
     @Override
-    public void autonomousInit() { }
+    public void autonomousInit() {
+        command = new XboxTestCommand();
+        command.start();
+    }
 
     @Override
     public void teleopInit() { }
@@ -23,10 +33,13 @@ public class Robot extends TimedRobot {
     public void disabledPeriodic() { }
     
     @Override
-    public void autonomousPeriodic() { }
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
     @Override
-    public void teleopPeriodic() { }
+    public void teleopPeriodic() {
+    }
 
     @Override
     public void testPeriodic() { }
